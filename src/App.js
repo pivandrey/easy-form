8708b5dd68dc3737a7770e920dc5d71b2f87d42e
pivandrey <pivandrey777@gmail.com>
2019-08-05 22:50:01 +0600

    create app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Field } from 'react-final-form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialValues = {
+  firstName: 'Brad',
+  lastName: 'Pitt',
+  patronym: 'William'
+}
+
+class App extends React.Component {
+
+  handleSubmit = async (values) => {
+    await console.log(values);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form
+          onSubmit={ this.handleSubmit }
+          initialValues={ initialValues }
+          render={ ({ handleSubmit, form }) => (
+            <form onSubmit={ event => {
+              console.log(form);
+              handleSubmit(event).then(() => {
+                form.initialize({})
+              })
+             } }>
+              <h2>Form</h2>
+              <div className="item">
+                <label>First Name</label>
+                <Field 
+                  name="firstName" 
+                  component="input" 
+                  placeholder="First Name" 
+                />
+              </div>
+              <div className="item">
+                <label>Last Name</label>
+                <Field 
+                  name="lastName" 
+                  component="input" 
+                  placeholder="Last Name" 
+                />
+              </div>
+              <div className="item">
+                <label>Patronym</label>
+                <Field 
+                  name="patronym" 
+                  component="input" 
+                  placeholder="Patronym" 
+                />
+              </div>
+              <button type="submit">
+                Send
+              </button>
+              <button
+                type="button"
+                onClick={ () => form.initialize({}) }
+              >
+                Clear
+              </button>
+              <button
+                type="button"
+                onClick={ () => form.initialize(initialValues) }
+              >
+                Return InitialValues
+              </button>
+            </form>
+          ) }
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
