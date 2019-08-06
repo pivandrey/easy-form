b77f28bd30bba4ad7e47e6357fc01d5aed73994e
pivandrey <pivandrey777@gmail.com>
2019-08-06 09:51:07 +0600

    add validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
-import './App.scss';
+import './App.css';
 
 const initialValues = {
   firstName: 'Brad',
@@ -8,65 +8,126 @@ const initialValues = {
   patronym: 'William'
 }
 
+const url = 'mock.address';
+
 class App extends React.Component {
 
   handleSubmit = async (values) => {
-    await console.log(values);
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(values)
+    });
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="root">
         <Form
           onSubmit={ this.handleSubmit }
           initialValues={ initialValues }
-          render={ ({ handleSubmit, form }) => (
-            <form onSubmit={ event => {
-              console.log(form);
-              handleSubmit(event).then(() => {
-                form.initialize({})
-              })
-             } }>
-              <h2>Form</h2>
-              <div className="item">
-                <label>First Name</label>
-                <Field 
-                  name="firstName" 
-                  component="input" 
-                  placeholder="First Name" 
-                />
+          validate={ values => {
+            const errors = {};
+            if (!values.firstName) {
+              errors.firstName = "Required";
+            }
+            if (!values.lastName) {
+              errors.lastName = "Required";
+            }
+            if (!values.patronym) {
+              errors.patronym = "Required";
+            } 
+            return errors;
+          } }
+          render={ ({ handleSubmit, form, values }) => (
+            <form
+              className="form"
+              onSubmit={ async (event) => {
+                await handleSubmit(event);
+                Object.keys(values).length === 3 && form.initialize({})
+              } }
+             >
+              <h2 className="header">Form</h2>
+              <div className="fields">
+                <div className="item">
+                  <label className="label">First Name</label>
+                  <Field 
+                    name="firstName" 
+                  >
+                    {({ input, meta }) => (
+                      <div className="inputWrapper">
+                        <input 
+                          {...input} 
+                          type="text" 
+                          placeholder="First Name"
+                          className="input" 
+                        />
+                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <div className="item">
+                  <label className="label">Last Name</label>
+                  <Field 
+                    name="lastName" 
+                  >
+                    {({ input, meta }) => (
+                      <div className="inputWrapper">
+                        <input 
+                          {...input} 
+                          type="text" 
+                          placeholder="Last Name"
+                          className="input" 
+                        />
+                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <div className="item">
+                  <label className="label">Patronym</label>
+                  <Field 
+                    name="patronym" 
+                  >
+                    {({ input, meta }) => (
+                      <div className="inputWrapper">
+                        <input 
+                          {...input} 
+                          type="text" 
+                          placeholder="Patronym"
+                          className="input" 
+                        />
+                        {meta.error && meta.touched && <span>{meta.error}</span>}
+                      </div>
+                    )}
+                  </Field>
+                </div>
               </div>
-              <div className="item">
-                <label>Last Name</label>
-                <Field 
-                  name="lastName" 
-                  component="input" 
-                  placeholder="Last Name" 
-                />
+              <div className="buttons">
+                <button 
+                  className="button"
+                  type="submit"
+                >
+                  Send
+                </button>
+                <button
+                  className="button"
+                  type="button"
+                  onClick={ () => form.initialize({}) }
+                >
+                  Clear
+                </button>
+                <button
+                  className="button"
+                  type="button"
+                  onClick={ () => form.initialize(initialValues) }
+                >
+                  Return InitialValues
+                </button>
               </div>
-              <div className="item">
-                <label>Patronym</label>
-                <Field 
-                  name="patronym" 
-                  component="input" 
-                  placeholder="Patronym" 
-                />
-              </div>
-              <button type="submit">
-                Send
-              </button>
-              <button
-                type="button"
-                onClick={ () => form.initialize({}) }
-              >
-                Clear
-              </button>
-              <button
-                type="button"
-                onClick={ () => form.initialize(initialValues) }
-              >
-                Return InitialValues
-              </button>
             </form>
           ) }
         />
